a5b431883e2051cfd4f048898038fd635fd22635
Andrew Smith <848smith@gmail.com>
2020-02-04 02:41:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,21 +7,14 @@ def third_challenge
         },
       :hero => {name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+      :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        :name => ["Lord Capulet"]
-        :age => ["50"]
+      :patriarch => {name: "Lord Capulet", age: "50"
         },
-      :matriarch => {
-        :name => ["Lady Capulet"]
-        :age => ["51"]
+      :matriarch => {name: "Lady Capulet", age: "51"
         },
-      :heroine => {
-        :name => ["Juliet"]
-        :age => ["15"]
-        :status => ["alive"]
+      :heroine => {name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
    }
